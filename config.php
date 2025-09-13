d5c9898736380cfc7decc3633ec68177a9bc6d73
Nikhil Bhanderi <nikhilbhanderi4410@gmail.com>
2025-09-13 23:30:02 +0530

    Update config.php

--- config.php
+++ config.php
@@ -5,6 +5,7 @@ $user = 'root';
 $pass = '';
 
 try {
+    
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
